d552000fb592564d1e6f0bd661b05410328de72f
sourav199 <souravdhar1999@gmail.com>
2023-02-27 22:42:39 -0500

    learning express.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,4 +2,20 @@ const express = require("express");
 
 const app = express();
 const PORT = 5000;
+
+//This will help us to log json data from endpoint
+app.use(express.urlencoded({ extended: false }));
+//@root api
+app.post("/", (req, res) => {
+  const { name, email } = req.body;
+  console.log(name);
+
+  if (!name || !email) {
+    res.send("Please fill out all the form");
+  } else {
+    console.log(name);
+    res.send(`data recieved thank you ${name}`);
+  }
+});
+//this .listen will start the application
 app.listen(PORT, () => console.log("server finished starting"));
