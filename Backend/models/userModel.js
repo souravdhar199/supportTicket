44fe2782c8142f123f3d93d6632ec10720e46ef0
sourav199 <souravdhar1999@gmail.com>
2023-03-02 23:04:58 -0500

    feat: Familared with mogoose.Schema() and .Model()

---
+++ Backend/models/userModel.js
@@ -0,0 +1,25 @@
+//@Making Schema for users which will stor user name passward etc.
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      require: [true, "Please enter your email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: [true, "Please enter your name"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//@Exporting the Schema ~ mongoose.Model(Name of the model, Created Schema)
+module.exports = mongoose.Model("User", userSchema);
