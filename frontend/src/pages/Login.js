1a252a2c0654ef3b51e03990b1988e2dd7efc20d
sourav199 <souravdhar1999@gmail.com>
2023-04-03 13:19:10 -0400

    Created Login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [formData, setFromdata] = new useState({
+    Password: "",
+    Email: "",
+  });
+
+  const { Password, Email } = formData;
+
+  // This function will take care of form subsmission
+  const onSubmits = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={onSubmits}>
+          <p>email</p>
+          <input
+            onChange={(e) =>
+              setFromdata({ ...formData, Email: e.target.value })
+            }
+            type="text"
+          />
+          <p>Type your Password</p>
+          <input
+            onChange={(e) =>
+              setFromdata({ ...formData, Password: e.target.value })
+            }
+            type="password"
+          />
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
